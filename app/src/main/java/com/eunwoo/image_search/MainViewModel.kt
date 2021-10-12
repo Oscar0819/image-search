026f488ee35d359d0862ae9295a196930654578f
Oscar <dn0963@naver.com>
2021-10-12 23:39:03 +0900

    add getImageSearch for MainViewModel remove DataModel to DataModelImpl

--- app/src/main/java/com/eunwoo/image_search/MainViewModel.kt
+++ app/src/main/java/com/eunwoo/image_search/MainViewModel.kt
@@ -1,12 +1,25 @@
 package com.eunwoo.image_search
 
+import android.util.Log
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
 class MainViewModel(private val model: DataModel): BaseKotlinViewModel() {
-    interface DataModel {
-        fun getData()
-    }
-    class DataModelImpl: DataModel{
-        override fun getData() {
-            return
-        }
+
+    private val TAG = "MainViewModel"
+
+    fun getImageSearch(query: String, page: Int, size:Int) {
+        addDisposable(model.getData(query, KakaoSearchSortEnum.Accuracy, page, size)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                it.run {
+                    if (documents.size > 0) {
+                        _imageSearchResponseLiveData.postValue(this)
+                    }
+                }
+            }, {
+                    Log.d(TAG, "response error, message : ${it.message}")
+            }))
     }
 }
