f6d2f7ecbadb354f1464f93417654d2c770614c9
Oscar <dn0963@naver.com>
2021-10-12 22:48:36 +0900

    add auth for KakaoSearchService

--- app/src/main/java/com/eunwoo/image_search/KakaoSearchService.kt
+++ app/src/main/java/com/eunwoo/image_search/KakaoSearchService.kt
@@ -8,6 +8,7 @@ import retrofit2.http.Query
 interface KakaoSearchService {
     @GET("/v2/search/image")
     fun searchImage (
+        @Header ("Authorization") auth: String,
         @Query ("query") query: String,
         @Query ("sort") sort: String,
         @Query ("page") page: Int,
