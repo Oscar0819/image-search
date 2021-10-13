184d02a584ae06d518e27d041e2ee5bf25921e6e
Oscar <dn0963@naver.com>
2021-10-13 22:05:57 +0900

    add imageSearchResponseLiveData for MainViewModel

--- app/src/main/java/com/eunwoo/image_search/MainViewModel.kt
+++ app/src/main/java/com/eunwoo/image_search/MainViewModel.kt
@@ -1,12 +1,18 @@
 package com.eunwoo.image_search
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 class MainViewModel(private val model: DataModel): BaseKotlinViewModel() {
 
     private val TAG = "MainViewModel"
+
+    private val _imageSearchResponseLiveData = MutableLiveData<ImageSearchResponse>()
+    val imageSearchResponseLiveData: LiveData<ImageSearchResponse>
+        get() = _imageSearchResponseLiveData
 
     fun getImageSearch(query: String, page: Int, size:Int) {
         addDisposable(model.getData(query, KakaoSearchSortEnum.Accuracy, page, size)
