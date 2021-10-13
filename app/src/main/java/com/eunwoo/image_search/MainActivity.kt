bdd9f4f41fdba3bae0c49b7bd13145ed31a7704b
Oscar <dn0963@naver.com>
2021-10-14 00:26:08 +0900

    add RecyclerView

--- app/src/main/java/com/eunwoo/image_search/MainActivity.kt
+++ app/src/main/java/com/eunwoo/image_search/MainActivity.kt
@@ -2,26 +2,44 @@ package com.eunwoo.image_search
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.eunwoo.image_search.databinding.ActivityMainBinding
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseKotlinActivity<ActivityMainBinding, MainViewModel>() {
     override val layoutResourceId: Int
         get() = R.layout.activity_main
 
-
     override val viewModel: MainViewModel by viewModel()
 
-    override fun initStartView() {
+    private val mainSearchRecyclerViewAdapter: MainSearchRecyclerViewAdapter by inject()
 
+    override fun initStartView() {
+        viewDataBinding.mainActivitySearchRecyclerView.run{
+            adapter = mainSearchRecyclerViewAdapter
+            layoutManager = StaggeredGridLayoutManager(3, 1).apply {
+                gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS
+                orientation = StaggeredGridLayoutManager.VERTICAL
+            }
+            setHasFixedSize(true)
+        }
     }
 
     override fun initDataBinding() {
-
+        viewModel.imageSearchResponseLiveData.observe(this, Observer {
+            it.document.forEach {document ->
+                mainSearchRecyclerViewAdapter.addImageItem(document.image_url, document.doc_url)
+            }
+            mainSearchRecyclerViewAdapter.notifyDataSetChanged()
+        })
     }
 
     override fun initAfterBinding() {
-
+        viewDataBinding.mainActivitySearchButton.setOnClickListener {
+            viewModel.getImageSearch(viewDataBinding.mainActivitySearchTextView.text.toString(), 1, 80)
+        }
     }
 
 }
